9d15868813a2b4879be35cb208084123ea58e1ec
guangli.lgl <guangli.lgl@alibaba-inc.com>
2017-04-24 21:58:44 +0800

    feat: test OkHttp

--- app/src/test/java/com/dalimao/mytaxi/ExampleUnitTest.java
+++ app/src/test/java/com/dalimao/mytaxi/ExampleUnitTest.java
@@ -15,3 +15,28 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 }
+
+
+
+
+
+/*private static Interceptor interceptor ＝ new Interceptor() {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            long t1 = System.nanoTime();
+            Request request = chain.request();
+
+            System.out.println(String.format("Sending request %s on %s%n%s",
+                    request.url(), chain.connection(), request.headers()));
+
+            Response response = chain.proceed(request);
+
+            long t2 = System.nanoTime();
+
+            System.out.println(String.format("Received response for %s in %.1fms%n%s",
+                    request.url(), (t2 - t1) / 1e6d, response.headers()));
+
+            return response;
+
+        }
+    };*/
